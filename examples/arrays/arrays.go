755c5410df0dab2d35fd47308f1ccfdc4b5334f7
liyalei <liyl04@missfresh.cn>
2019-02-25 13:21:12 +0800

    优化数组遍历

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -28,10 +28,10 @@ func main() {
     // 数组类型是一维的，但是你可以组合
     // 构造多维的数据结构。
     var twoD [2][3]int
-    for i := 0; i < 2; i++ {
-        for j := 0; j < 3; j++ {
-            twoD[i][j] = i + j
-        }
-    }
+    for i := 0; i < len(twoD); i++ {
+		for j := 0; j < len(twoD[i]); j++ {
+			twoD[i][j] = i + j
+		}
+	}
     fmt.Println("2d: ", twoD)
 }
