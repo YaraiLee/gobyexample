f17b0485464d1071e02e60f5ce6a1a0c99e92017
liyalei <liyalei@tusdao.com>
2020-03-23 11:15:52 +0800

    redis示例由于redis包过时，代码注释

--- examples/methods/methods.go
+++ examples/methods/methods.go
@@ -5,39 +5,39 @@ package main
 import "fmt"
 
 type rect struct {
-	width, height int
-	eleArea int
-	elePerimeter int
+    width, height int
+    eleArea       int
+    elePerimeter  int
 }
 
 // 这里的 `area` 方法有一个_接收器(receiver)类型_ `rect`。
 func (r *rect) area() int {
-	r.eleArea = r.width * r.height
-	return r.eleArea
+    r.eleArea = r.width * r.height
+    return r.eleArea
 }
 
 // 可以为值类型或者指针类型的接收器定义方法。这里是一个
 // 值类型接收器的例子。
 func (r rect) perim() int {
-	r.elePerimeter = 2* (r.width + r.height)
-	return r.elePerimeter
+    r.elePerimeter = 2 * (r.width + r.height)
+    return r.elePerimeter
 }
 
 func main() {
-	r := rect{width: 10, height: 5}
+    r := rect{width: 10, height: 5}
 
-	// 这里我们调用上面为结构体定义的两个方法。
-	fmt.Println("area: ", r.area())
-	fmt.Println(r.eleArea)
-	fmt.Println("perim:", r.perim())
-	fmt.Println(r.elePerimeter)
+    // 这里我们调用上面为结构体定义的两个方法。
+    fmt.Println("area: ", r.area())
+    fmt.Println(r.eleArea)
+    fmt.Println("perim:", r.perim())
+    fmt.Println(r.elePerimeter)
 
-	// Go 自动处理方法调用时的值和指针之间的转化。你可以使
-	// 用指针来调用方法来避免在方法调用时产生一个拷贝，或者
-	// 让方法能够改变接受的机构体。
-	rp := &r
-	fmt.Println("area: ", rp.area())
-	fmt.Println(r.eleArea)
-	fmt.Println("perim:", rp.perim())
-	fmt.Println(r.elePerimeter)
+    // Go 自动处理方法调用时的值和指针之间的转化。你可以使
+    // 用指针来调用方法来避免在方法调用时产生一个拷贝，或者
+    // 让方法能够改变接受的机构体。
+    rp := &r
+    fmt.Println("area: ", rp.area())
+    fmt.Println(r.eleArea)
+    fmt.Println("perim:", rp.perim())
+    fmt.Println(r.elePerimeter)
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module gobyexample
+
+go 1.13
+
+require (
+	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40
+	github.com/bmizerany/pq v0.0.0-20131128184720-da2b95e392c1
+	github.com/russross/blackfriday v2.0.0+incompatible
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+)
