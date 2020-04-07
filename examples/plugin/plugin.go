8ad22672d74ebafa2f387b5a01f1f15c03c2a244
liyalei <liyalei@tusdao.com>
2020-04-07 09:56:35 +0800

    [Add] add plugin example

---
+++ examples/plugin/plugin.go
@@ -0,0 +1,13 @@
+/**
+ * @Author: liyalei
+ * @Description:
+ * @Version:
+ * @Date: 2020/4/3 6:35 下午
+ */
+package main
+
+import "fmt"
+
+var V int
+
+func F() { fmt.Printf("Hello, number %d\n", V) }
